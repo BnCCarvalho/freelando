f50fa0917e3a97a1b082a90c535ebd5723341738
CARVALHO <CAIOTULIO.CARVALHO@mosaicco.com>
2023-10-10 13:54:35 -0300

    criacao do card e colocacao dos temas

---
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "@emotion/react";
+
+const tema = {
+  cores: {
+    branco: "#E8E8E8",
+    atencao: "#FF0E0E",
+    focus: "#B009FF",
+    primarias: {
+      a: "#5754ED",
+      b: "#D93114",
+      c: "#168070",
+    },
+    secundarias: {
+      a: "#EBEAF9",
+      b: "#FDF8F8",
+      c: "#EBFCF9",
+    },
+    neutras: {
+      a: "#373737",
+      b: "#E8E8E8",
+      c: "#F5F5F5",
+    },
+    dark: {
+      a: "#110EA0",
+      b: "#B61B00",
+    },
+  },
+  espacamentos: {
+    s: "16px",
+    l: "32px",
+  },
+};
+
+export const ProvedorTema = ({ children }) => {
+  return <ThemeProvider theme={tema}>{children}</ThemeProvider>;
+};
